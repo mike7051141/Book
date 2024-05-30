7594f824a5801a62f07684395150b10b87e32ba1
choiht0904 <choiht0904@naver.com>
2024-05-30 16:18:02 +0900

    create entity review

--- src/main/java/com/example/Book/entity/Review.java
+++ src/main/java/com/example/Book/entity/Review.java
@@ -1,4 +1,22 @@
 package com.example.Book.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "REVIEW_ID")
+    private Long id;
+
+    @Column(name = "REVIEW_USER_NAME")
+    private String userName;
+
+    @Column(name = "REVIEW_CONTENT")
+    private String content;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
 }
