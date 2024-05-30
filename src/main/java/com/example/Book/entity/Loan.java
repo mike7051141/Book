3e68dbdef3190a1e0885ee56883bf98a25895329
ggowater112@naver.com <127701730+haennni@users.noreply.github.com>
2024-05-30 16:27:42 +0900

    Loan_Entity

--- src/main/java/com/example/Book/entity/Loan.java
+++ src/main/java/com/example/Book/entity/Loan.java
@@ -1,4 +1,40 @@
 package com.example.Book.entity;
 
+import com.example.Book.entity.Book;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
 public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Book bookId;
+
+    @Column
+    private LocalDateTime loanDate;
+
+    @Column
+    private LocalDateTime returnDate;
+
+    @Column
+    private Status status;
+
+    @Column
+    private String borrowerName;
+
+    @Column
+    private String borrowerPhoneNumber;
+
 }
