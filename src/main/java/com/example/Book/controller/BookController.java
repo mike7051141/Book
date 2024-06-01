79102d502cc91fbb84b2814d8b00b1661242dd0c
mike7051141 <mike705114@gmail.com>
2024-06-01 14:34:10 +0900

    book delete 기능 추가

--- src/main/java/com/example/Book/controller/BookController.java
+++ src/main/java/com/example/Book/controller/BookController.java
@@ -6,32 +6,30 @@ import com.example.Book.serivce.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/books/add")
+    @GetMapping("/add")
     public String showAddBookForm(Model model) {
         model.addAttribute("book", new BookDto());
         return "addBook";
     }
 
-    @PostMapping("/books/add")
+    @PostMapping("/add")
     public String addBook(@ModelAttribute("book") BookDto bookDto) {
         bookService.save(bookDto);
         return "redirect:/books";
     }
 
-    @GetMapping("/books")
+    @GetMapping
     public String listBooks(Model model) {
         model.addAttribute("books", bookService.findAll());
         return "bookList";
@@ -50,5 +48,11 @@ public class BookController {
         }
         model.addAttribute("books", books);
         return "searchResults";
+    }
+
+    @GetMapping("/delete/{idx}")
+    public  String delete(@PathVariable int idx) {
+        bookService.delete(idx);
+        return "redirect:/books";
     }
 }
